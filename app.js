86fb832d108e77a4f35ba2aacc754b8f28e770d9
Matheus E. Muller <hello@memuller.com>
2019-08-16 13:15:18 -0300

    chore: shares DB Flights collection w/ express routes

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express")
-const PORT = process.env['PORT'] || 3000
+const bodyParser = require('body-parser')
+const mongo = require('./lib/db')
+const FlightsDB = require('./models/Flight')
+const FlightsRouter = require("./routes/flights")
 
-const flightsRouter = require("./routes/flights")
+const PORT = process.env['PORT'] || 3000
 
 const app = express()
 
-app.use('/flights', flightsRouter)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.listen(PORT, () => 'app is up!')
+app.use('/flights', FlightsRouter)
+
+mongo.db().then( (connection) => {
+  FlightsDB.setCollection(connection.flights)
+  app.locals.Flight = FlightsDB.Flight
+  app.listen(PORT, () => 'app is up!')
+})
